85426336218d77e0a2860c3203dc710f5ce290f6
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-26 14:16:31 +0100

    Update new App.jsx

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,8 +1,17 @@
+import Home from "../components/home/home";
+import Navbar from "../components/navbar/navbar";
+import backgroundImage from "../assets/quiz-bg.png";
+import "./App.css";
+
 export default function Root() {
   return (
     <>
       <div>
-        <p>HOME PAGE</p>
+        <div className="background">
+          <Navbar />
+          <img src={backgroundImage}></img>
+          <Home />
+        </div>
       </div>
     </>
   );
