da459ff2f83cebeecc755f9f0ffc428568be3eb2
Juliana Nocchi Dobal <junocchi@gmail.com>
2023-09-26 14:28:36 +0100

    Added image to the background

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,17 +1,13 @@
 import Home from "../components/home/home";
 import Navbar from "../components/navbar/navbar";
-import backgroundImage from "../assets/quiz-bg.png";
 import "./App.css";
 
 export default function Root() {
   return (
     <>
-      <div>
-        <div className="background">
-          <Navbar />
-          <img src={backgroundImage}></img>
-          <Home />
-        </div>
+      <div className="background">
+        <Navbar />
+        <Home />
       </div>
     </>
   );
